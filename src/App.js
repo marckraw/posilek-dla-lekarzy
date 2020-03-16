9c307ab1009df4c2feee944c8e2879953b13e247
Marcin Krawczyk <marckraw@icloud.com>
2020-03-16 20:50:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./main.scss";
+
+function App() {
+    return (
+        <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+            <h3 className="uk-card-title">Default</h3>
+            <p>
+                Lorem ipsum <a href="#">dolor</a> sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+            </p>
+        </div>
+    );
+}
+
+export default App;
